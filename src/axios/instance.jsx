3f222944ddad82d24cf13628b3d9023b283b24ef
k-mat438 <a1sco00ocs1a@gmail.com>
2024-06-25 20:18:44 +0900

    json形式に変更

--- src/axios/instance.jsx
+++ src/axios/instance.jsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const apiHome = axios.create({
-  baseURL: `${process.env.REACT_APP_REQUEST_URL}/api/v1`
+  baseURL: `${process.env.REACT_APP_REQUEST_URL}/api/v1`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 
 export const registerInstance = async(data) =>{
   data.confirm_success_url = `${process.env.REACT_APP_CONFIRM_SUCCESS_URL}`;
-  const response = await apiHome.post('/users', new URLSearchParams(data));
+  const response = await apiHome.post('/users', data);
   
   return response.data
 }
