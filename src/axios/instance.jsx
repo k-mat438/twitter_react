ce05d2a1642258bde5d9b75082efdd42805c33a5
k-mat438 <a1sco00ocs1a@gmail.com>
2024-07-10 21:46:06 +0900

    修正

--- src/axios/instance.jsx
+++ src/axios/instance.jsx
@@ -8,6 +8,15 @@ const apiHome = axios.create({
   },
 });
 
+const apiPrivate = axios.create({
+  baseURL: `${process.env.REACT_APP_REQUEST_URL}/api/v1`,
+  headers: {
+    'Content-Type': 'application/json',
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid")
+  },
+})
 
 export const registerInstance = async(data) =>{
   const response = await apiHome.post('/users',data);
@@ -22,20 +31,15 @@ export const loginInstance = async(data) => {
 }
 
 
-export const loginOutstance = async(data) => {
+export const logOutInstance = async(data) => {
   const response = await apiHome.post('/users/sign_out', data);
 
   return response.data
 }
 
 export const getCurrentUser = async() => {
-  if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) return
-  const currentUser = await apiHome.get('/auth/sessions', {
-    headers: {
-      "access-token": Cookies.get("_access_token"),
-      "client": Cookies.get("_client"),
-      "uid": Cookies.get("_uid")
-    }
-  })
+  if (!Cookies.get("_access_token") || !Cookies.get("_client") || !Cookies.get("_uid")) 
+    return null
+  const currentUser = await apiPrivate.get('/auth/sessions')
   return currentUser.data
 }
